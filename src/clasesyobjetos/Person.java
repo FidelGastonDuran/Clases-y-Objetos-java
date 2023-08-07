d28fc1e0c3acc6f8654129163d99178c258a835d
Fidel Duran <you@example.com>
2023-08-07 00:10:19 -0300

    !

--- src/clasesyobjetos/Person.java
+++ src/clasesyobjetos/Person.java
@@ -2,4 +2,36 @@ package clasesyobjetos;
 
 public class Person {
 
+	/*Atributos*/
+	private String nombre;
+	private String apellido;
+	
+	/*Constructor*/
+	public Person(String nombre, String apellido) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
+
+	/*Getters and Setters*/
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	
+	
+	
+	
 }
